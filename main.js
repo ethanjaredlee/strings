5c388c1e2e14d3bcede15428f0767948fbe094b4
ethlee4 <ethanlee2020@u.northwestern.edu>
2017-08-03 20:59:03 -0500

    added point to diverge to

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 var strings = [];
+var setter;
+var drawSetter;
 
 function setup() {
     createCanvas(600, 600);
     background(160);
+    count = 0; //used to determine when to add a new line
 }
 
 function draw() {
@@ -11,21 +14,35 @@ function draw() {
     fill(200);
     mousePos = createVector(mouseX - width/2, mouseY - height/2);
 
-    s = new String(diameter, createVector(0,0));
-    strings.push(s);
+    count++;
+    if (count % 50 == 0) {
+        s = new String(diameter, createVector(0,0));
+        strings.push(s);
+    }
 
     for (i = 0; i<strings.length; i++) {
         strings[i].crawl();
         strings[i].display();
         if (strings[i].outOfRange()) {
             strings.splice(i, 1);
-            console.log('spliced');
         }
     }
 
-    console.log(strings.length);
     if (keyIsPressed) {
         background(160);
+        strings = [];
+    }
+
+    if (drawSetter) {
+        noStroke();
+        ellipse(setter.x, setter.y, 7, 7);
+    }
+}
+
+function mouseClicked() {
+    if (Math.abs(mousePos.x) < diameter/2 && Math.abs(mousePos.y) < diameter/2 && !drawSetter) {
+        setter = createVector(mousePos.x, mousePos.y);
+        drawSetter = true;
     }
 }
 
--- string.js
+++ string.js
@@ -9,18 +9,27 @@ function String(d, p) {
     this.finale = createVector(0,0);
     var direction = p5.Vector.sub(this.start, p).normalize();
     this.velocity = p5.Vector.mult(direction, 1);
+    this.c = color(random(0,255), random(0, 255), random(0, 255));
 
     this.crawl = function() {
         this.end.add(this.velocity);
     }
 
     this.display = function() {
+        stroke(this.c);
+        strokeWeight(5);
         line(this.start.x, this.start.y, this.end.x, this.end.y);
     }
 
     this.outOfRange = function() {
         if (Math.abs(this.end.x) > width/2 ||
             Math.abs(this.end.y) > height) {
+            return true;
+        }
+    }
+
+    this.circumference = function() {
+        if ((this.end.x)^2 + (this.end.y)^2 > 1) {
             return true;
         }
     }
